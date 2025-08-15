945b6e761b6f1adef295aacfa74fe60533f71a7b
Evan Ganske <evanganske1@gmail.com>
2025-08-15 11:57:57 -0500

    Initial Commit

---
+++ public/app.js
@@ -0,0 +1,90 @@
+// first getting variables to hold the form and table components
+const form = document.getElementById('transactionForm');
+const tableBody = document.querySelector('#transactionsTable tbody');
+const removeButton = document.getElementById('removeButton');
+
+// gets all of the table data and displays transactions
+function getTransactions() {
+    fetch("/transactions")
+    .then(response => {
+        if (!response.ok) { // check if response was good
+            throw new Error ("Server response had an error.");
+        } return response.json();
+    }).then(data => {
+        // data now contains the array full of transaction objects
+        console.log(data);
+
+        // remove any data from table from past requests
+        tableBody.innerHTML = "";
+
+        // now for each item in array, add as row to table
+        for (let i=0; i<data.length; i++) {
+            const newRow = tableBody.insertRow();
+
+            // putting data into the table
+            const idCell = newRow.insertCell(0);
+            idCell.textContent = data[i].id;
+            const dateCell = newRow.insertCell(1);
+            dateCell.textContent = data[i].date;
+            const descriptionCell = newRow.insertCell(2);
+            descriptionCell.textContent = data[i].description;
+            const amountCell = newRow.insertCell(3);
+            amountCell.textContent = data[i].amount;
+            const categoryCell = newRow.insertCell(4);
+            categoryCell.textContent = data[i].category_name;
+        }
+    });
+}
+
+// listening to form to tell when submit occurs
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); // prevent page reload when form submitted
+
+    // now need to get the data out of the form
+    var transaction = {
+        date: document.getElementById('date').value,
+        description: document.getElementById('description').value,
+        amount: parseFloat(document.getElementById('amount').value),
+        category: document.getElementById('category').value
+    };
+
+    // now need to send this new transaction to the server
+    fetch("/transactions", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(transaction)
+    }).then(response => {
+        if (!response.ok) { // check if response was good
+            throw new Error ("Server response had an error.");
+        } return response.json();
+    }).then(data => {
+        // add new row to the html table
+        const newRow = tableBody.insertRow();
+
+        // putting data into the table
+        const idCell = newRow.insertCell(0);
+        idCell.textContent = data.id;
+        const dateCell = newRow.insertCell(1);
+        dateCell.textContent = data.date;
+        const descriptionCell = newRow.insertCell(2);
+        descriptionCell.textContent = data.description;
+        const amountCell = newRow.insertCell(3);
+        amountCell.textContent = data.amount;
+        const categoryCell = newRow.insertCell(4);
+        categoryCell.textContent = data.category_name;
+    });
+});
+
+// listening to remove button
+removeButton.addEventListener("click", () => {
+    fetch("/transactions", {
+        method: "DELETE"
+    }).then(response => {
+        if (!response.ok) { // check if response was good
+            throw new Error ("Error deleting transaction data.");
+        } return response.json();
+    }).then(() => getTransactions());
+});
+
+// This should run immediately when the webpage is initially loaded up
+getTransactions();
---
+++ server.js
@@ -0,0 +1,96 @@
+// importing express and sqlite3
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+// getting an instance to be able to use express to communicate
+const app = express();
+app.use(express.json()); // automatically parses json when pulling in request
+app.use(express.static('public')); // making sure that everything in public folder is initially loaded
+
+// loading the main page (ALTERNATIVE APPROACH TO MANUALLY LOAD THE HTML HOME SCREEN)
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/public/index.html");
+// });
+
+// now setting up the database to use
+const db = new sqlite3.Database("database.db");
+
+// now creating tables for database to hold
+db.serialize(() => { // serialize is used to ensure that tables are created in proper order by db
+    // first creating the categories table
+    db.run("CREATE TABLE IF NOT EXISTS Categories (id INTEGER PRIMARY KEY, name TEXT UNIQUE)");
+
+    // now creating the transactions table
+    db.run(`CREATE TABLE IF NOT EXISTS Transactions 
+            (id INTEGER PRIMARY KEY,
+            date TEXT,  
+            description TEXT,
+            amount REAL,
+            category_id INTEGER,
+            FOREIGN KEY (category_id) REFERENCES Categories (id))`);
+});
+
+// get all transactions from the database
+app.get("/transactions", (req, res) => {
+    // set up sql query to get all transactions
+    db.all(`SELECT t.id, t.date, t.description, t.amount, c.name AS category_name
+            FROM Transactions t
+            JOIN Categories c
+            ON t.category_id = c.id`, (err, rows) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        res.json(rows);
+    });
+});
+
+// posting a transaction to the database
+app.post("/transactions", (req, res) => {
+    // first get data that was passed in from request
+    var date = req.body.date;
+    var description = req.body.description;
+    var amount = req.body.amount;
+    var category = req.body.category;
+
+    // insert the category 
+    db.run("INSERT OR IGNORE INTO Categories (name) VALUES (?)", [category], function(err) {
+        if (err) {console.error(err.message); return;}
+
+        // now get the category_id from category
+        db.get("SELECT id FROM Categories WHERE name = ?", [category], (err, data) => {
+            if (err) {console.error(err.message); return;}
+
+            // now insert the transaction
+            db.run("INSERT INTO Transactions (date, description, amount, category_id) VALUES (?, ?, ?, ?)", [date, description, amount, data.id], function(err){
+                if (err) {console.error(err.message); return;}
+                else { // sending response to client
+                    // getting latest entry out of db to send back to client for table update
+                    db.get(`SELECT t.id, t.date, t.description, t.amount, c.name AS category_name
+                            FROM Transactions t
+                            JOIN Categories c
+                            ON t.category_id = c.id
+                            ORDER BY t.id DESC LIMIT 1`, 
+                            (err, row) => {
+                            if (err) {console.error(err.message); return;}
+                            else {
+                                res.json(row);
+                            }
+                    });
+                } 
+            });
+        });
+    });
+});
+
+// deleting all transactions from the database
+app.delete("/transactions", (req, res) => {
+    // sql query to delete all rows
+    db.run("DELETE FROM Transactions", function(err) {
+        if (err) {console.error(err.message); return;}
+        else {res.json({success : true});}
+    })
+});
+
+
+app.listen(3000, () => console.log('Server running on http://localhost:3000'));
